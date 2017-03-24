629574e51e164458746b6a602b2e40ffbdb159db
Ni Tongyang <ntyaiqingr@gmail.com>
2017-03-23 23:38:23 -0400

    basic of python

---
+++ first_program.py
@@ -0,0 +1,11 @@
+print 'Hello world'
+print 'Hello'
+# This is just a comment
+print ('Hello world, I love you')
+
+s = 'Hello world'
+print (s)
+x = s[::5]
+print (x)
+print 'First %s, Second %s, Third %s' %('Chen', 'Tian', 'Qi')
+print 'I love {x} {y} {z}'.format(x = 'Chen', y = 'Tian', z = 'Qi')
