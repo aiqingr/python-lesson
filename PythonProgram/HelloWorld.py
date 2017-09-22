3a2de504506abcd90f1588d5f22e54198d0a6963
Ni Tongyang <ntyaiqingr@gmail.com>
2017-09-22 01:50:33 -0400

    print several strings together

--- PythonProgram/HelloWorld.py
+++ PythonProgram/HelloWorld.py
@@ -1,1 +1,13 @@
-print('Hello World');
+print('Hello World')
+print(1 + 2)
+print(2 + 34)
+print()
+print("The End")
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello " + "world")
+name = "Tongyang Ni"
+action = "love"
+lover = "Tianqi Chen"
+print(name + " " + action + " " + lover)
+
