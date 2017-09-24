9afb5c9dc99f867a9598b5076730d80aaa3f134d
Ni Tongyang <ntyaiqingr@gmail.com>
2017-09-23 22:46:08 -0400

    pratice the string

--- PythonProgram/HelloWorld.py
+++ PythonProgram/HelloWorld.py
@@ -10,8 +10,8 @@ name = "Tongyang Ni"
 action = "love"
 lover = "Tianqi Chen"
 print(name + " " + action + " " + lover)
-yourName = input("What is your name: ")
-print("My name is " + yourName)
+#yourName = input("What is your name: ")
+#print("My name is " + yourName)
 # This is just comment, and this will not display on the console
 a = 12
 b = 3
@@ -22,4 +22,30 @@ print(a / b)
 print(a // b)
 print(a * b)
 print(a % b)
-
+girlfriend = "Chen Tianqi"
+print(girlfriend)
+print(girlfriend[0])
+print(girlfriend[3])
+print(girlfriend[0:6])
+print(girlfriend[6:])
+print(girlfriend[-1])
+print(girlfriend[-4:-2])
+# [0:6:2] means print from index0 to index6 without index6 every two letters
+#that means print index0 not index1, index2, print index 3 not index4, index5
+#if we print including index6, the index6 will be printed  in the console
+print(girlfriend[0:6:2])
+print(girlfriend[0:6:3])
+number = "9,344,141,343,353,234,564"
+#[1::4] means [1:length:4] the detail meaning refer last comment
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6"
+print(numbers[0::3])
+string1 = "Nick's"
+string2 = "wife"
+print( string1 + string2 + girlfriend)
+print("day")
+today = "firday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("par" in "paarrot")
