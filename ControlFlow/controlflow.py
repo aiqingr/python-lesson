5d754918c42a0207434a4d5f57698a86c8002bdd
Ni Tongyang <ntyaiqingr@gmail.com>
2017-09-27 18:11:13 -0400

    if control flow

--- ControlFlow/controlflow.py
+++ ControlFlow/controlflow.py
@@ -1,7 +1,8 @@
- for i in range(1, 12):
+for i in range(1, 12):
     print(i)
     #The next two lines will inside the for loop
     print(i**2)
     print("Hello")
+    print("Woo, I am not sure what is the most important thing in the for loop")
 #The next line will not in the for loop
 print("Hello World")
---
+++ ifControlFlow/ifControlFlow.py
@@ -0,0 +1,11 @@
+name = input("Please enter your name: ")
+#please notice the next line, the int make the age as an integer
+#this can make ">=" symbol working, ">=" can work only between two numbers
+
+age = int(input("How old are you, {0}?".format(name)))
+print(age)
+if age >= 18:
+	print("You are old enough to vote")
+	print("Please put an X in the bos")
+else:
+	print("Please come back in {0} years".format(18 - age))
