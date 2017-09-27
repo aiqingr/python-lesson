035aeb554b6daa7f48780894f705a99195fa8bc3
Ni Tongyang <ntyaiqingr@gmail.com>
2017-09-27 00:35:22 -0400

    controlflow

---
+++ ControlFlow/controlflow.py
@@ -0,0 +1,7 @@
+ for i in range(1, 12):
+    print(i)
+    #The next two lines will inside the for loop
+    print(i**2)
+    print("Hello")
+#The next line will not in the for loop
+print("Hello World")
