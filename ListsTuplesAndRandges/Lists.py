27e70d84395dfec3194e8e9ce31cca056254ac55
Ni Tongyang <ntyaiqingr@gmail.com>
2017-10-03 20:07:43 -0400

    basic lists characters

---
+++ ListsTuplesAndRandges/Lists.py
@@ -0,0 +1,24 @@
+# ipAddress = input("Please enter an IP address: ")
+# print(ipAddress.count("."))
+parrot_list = ["non pinin'", "no more", "a stiff", "bereft of live"]
+parrot_list.append("A Norwegian Blue")
+
+for state in parrot_list:
+    print("This parrot is " + state)
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7]
+numbers = even + odd
+number_in_order = sorted(numbers)
+
+print(number_in_order)
+
+if numbers == number_in_order:
+    print("The lists are equal")
+else:
+    print("The lists are not equal")
+
+if number_in_order == sorted(numbers):
+    print("The lists are equal")
+else:
+    print("The lists are not equal")
