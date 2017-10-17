c172d906289a87ffec403bfcc052d6a29e849982
Ni Tongyang <ntyaiqingr@gmail.com>
2017-10-17 00:55:31 -0400

    tuple advanced

---
+++ ListsTuplesAndRandges/tuple2.py
@@ -0,0 +1,22 @@
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad company", "Bad company", 1974
+budgie = "Nightflight", "budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lighting", "Metallica", 1984
+
+metallica2 = ["Ride the Lighting", "Metallia", 1983]
+print(metallica2)
+
+# metallica2.append("Rock")
+title, artist, year = metallica2
+print(title)
+print(artist)
+print(year)
+
+song = 1, 2, 3, 4, 5
+t1, t2, t3, t4, t5 = song
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+print(t5)
