43ab8b880eb712b3868db0f9d37d61d993c70351
Ni Tongyang <ntyaiqingr@gmail.com>
2017-10-17 20:48:05 -0400

    first touch of dictionary

---
+++ dictionaryAndSets/dictionaries.py
@@ -0,0 +1,6 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+print(fruit)
+print(fruit["lime"])
