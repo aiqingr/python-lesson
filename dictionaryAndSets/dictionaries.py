77dd6a1f9828fe51e224e1884de47041222a5138
Ni Tongyang <ntyaiqingr@gmail.com>
2017-10-18 20:29:29 -0400

    dictionary learning

--- dictionaryAndSets/dictionaries.py
+++ dictionaryAndSets/dictionaries.py
@@ -2,5 +2,28 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "apple": "good for making cider",
          "grape": "a small, sweet fruit growing in bunches",
          "lime": "a sour, green citrus fruit"}
+# there is no error in the console if we have duplicate key, however
+# there will be a warning
+# "apple": "round and crunchy"
 print(fruit)
 print(fruit["lime"])
+fruit["pear"] = "an odd shaped apple"
+print(fruit)
+fruit["pear"] = "great with tequila"
+print(fruit)
+# del fruit["lime"]
+# print(fruit["lime"])
+# del fruit
+# fruit.clear()
+# print(fruit)
+# print(fruit["tomato"])
+print(fruit)
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("We don't have that fruit")
