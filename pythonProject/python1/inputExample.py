f1212ba148e0a7e29a979d978e2b53bac0f38d82
Ni Tongyang <ntyaiqingr@gmail.com>
2018-07-04 23:38:33 -0400

    input function trick

---
+++ pythonProject/python1/inputExample.py
@@ -0,0 +1,10 @@
+# num_input = input("Input a number")
+# print(num_input ** 2)
+# First two line will popup an error because the input function always return a string
+
+# This will be Method One
+# num_input_1 = input("input a number: ")
+# print(int(num_input_1) ** 2)
+
+num_input_2 = int(input("input a number: "))
+print(num_input_2 ** 2)
