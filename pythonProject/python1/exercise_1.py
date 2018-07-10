418dd5b0a14886ad1c8d411b58c4a17e8102ac51
Ni Tongyang <ntyaiqingr@gmail.com>
2018-07-10 00:07:52 -0400

    modify the if statement

--- pythonProject/python1/exercise_1.py
+++ pythonProject/python1/exercise_1.py
@@ -1,7 +1,8 @@
 temp = [10,-20,-289,100]
 def c_to_f(c):
     if c < -273.15:
-        print("Temprature can not be less than 273.15 Celsius")
+        # print("Temprature can not be less than 273.15 Celsius")
+        return "emprature can not be less than 273.15 Celsius"
     else:
         return c*9/5+32
 
