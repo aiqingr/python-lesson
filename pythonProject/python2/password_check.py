b5e9c07cff98e86caa02659434294553b08886b2
Ni Tongyang <ntyaiqingr@gmail.com>
2018-07-13 23:16:37 -0400

    password check system

--- pythonProject/python2/password_check.py
+++ pythonProject/python2/password_check.py
@@ -1,7 +1,9 @@
 correct_password = "python123"
+name = input("Enter name: ")
 password = input("Enter password: ")
 
 while correct_password != password:
     password = input("Wrong password! Enter again: ")
 
-print("Logged in!")
+# print("Hi", name, "you are logged in!")
+print("Hi %s you are logged in!" % name)
