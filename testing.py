f55c177edcca0aebb66e021597959d6a1e91c146
aiqingr <ntyaiqingr@gmail.com>
2019-11-11 18:17:28 -0500

    fixed

--- testing.py
+++ testing.py
@@ -1,5 +1,17 @@
-print("testing")
+import sys
+print(sys.executable)
+print(sys.version)
 
-print("again")
+for num in [1, 2, 3, 4]:
+    print(num)
 
-print("testing")
+for numer in [12, 3, 14, 15]:
+    print(numer)
+
+
+class ClassName(object):
+    """docstring for ClassName"""
+
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
